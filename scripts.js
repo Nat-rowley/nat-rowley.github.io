5254601d90996d46cc57bd4d369c3367c3e7ba78
Nat-rowley <106767893+Nat-rowley@users.noreply.github.com>
2024-07-03 21:13:18 +0100

    Portfolio Update

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+event.preventDefault();
+
+const inputs = document.querySelectorAll('#contactForm input[aria-required="true"], #contactForm textarea[aria-required="true"]');
+let isValid = true;
+
+inputs.forEach(function(input) {
+    const errorElement = document.getElementById(input.getAttribute('aria-describedby'));
+
+    if (input.ariaValueMax.trim() === '') {
+        input.setAttribute('aria-invalid', 'true');
+        errorElement.textContent = input.previousElementSibling.textContent.trim() + ' is required';
+        isValid = false;
+    } else {
+        input.removeAttribute('aria-invalid');
+        errorElement.textContent = '';
+    }
+});
+
+if (isValid) {
+    this.submit();
+
+}
+
+});
