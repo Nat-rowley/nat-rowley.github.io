1d94456bb2be36435e4291dae5e9d564dd885555
Nat-rowley <106767893+Nat-rowley@users.noreply.github.com>
2024-07-05 14:51:10 +0100

    Portfolio Update

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
 
     const inputs = document.querySelectorAll('#contactForm input[aria-required="true"], #contactForm textarea[aria-required="true"]');
     let isValid = true;
+    const errorMessages = [];
 
     inputs.forEach(function(input) {
         const errorElement = document.getElementById(input.getAttribute('aria-describedby'));
@@ -11,6 +12,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         if (input.value.trim() === '') {
             input.setAttribute('aria-invalid', 'true');
             errorElement.textContent = labelText + ' is required';
+            errorMessages.push(labelText + ' is required');
             isValid = false;
         } else {
             input.removeAttribute('aria-invalid');
@@ -18,8 +20,17 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         }
     });
 
+    const formSummary = document.getElementById('form-summary');
+    formSummary.innerHTML = '';
+
     if (isValid) {
+        formSummary.classList.remove('error-summary');
+        formSummary.classList.add('success-summary');
+        formSummary.innerHTML = '<i class="ph-fill ph-check-circle"></i> Contact Form Success. Thanks for contacting me ';
         this.submit();
     } else {
+        formSummary.classList.remove('success-summary');
+        formSummary.classList.add('error-summary');
+        formSummary.innerHTML = '<i class="ph-fill ph-warning"></i> Contact Form Failed. ' + errorMessages.join(' ');
     }
 });
